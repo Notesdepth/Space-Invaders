de1c213c49563ca766172f27fc8bd50f688ba983
Notesdepth <cedric.tesse@laposte.net>
2022-06-14 10:07:13 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import com.googlecode.lanterna.terminal.Terminal;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Terminal ter;
+        System.out.println("test");
+    }
+}
